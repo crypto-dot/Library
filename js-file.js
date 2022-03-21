748ba2b92c543829780a0d331e4fa0da9348c561
Carlos Arbizu <carlos2345532@gmail.com>
2022-03-20 23:43:02 -0500

    gave modal the ability to add books to the library updated book class

--- js-file.js
+++ js-file.js
@@ -1,24 +1,45 @@
 const addBookElement = document.querySelector(".addBook");
 const modalWindow = document.querySelector(".modal");
-let library = [];
+const form = document.querySelector("form");
+const divContainingBy = document.createElement("div");
+divContainingBy.textContent = "By";
 
-function Book(nameBook,author,title,numberOfPages,haveRead){
-    this.nameBook = nameBook;
-    this.author = author;
-    this.title = title;
-    this.numberOfPages = numberOfPages;
-    this.haveRead = haveRead;
-}
-Book.prototype.info = function() {
-    return `${this.nameBook} ${this.author} ${this.title} ${this.numberOfPages} ${this.haveRead}`;
+let library = document.querySelector(".library");
+let libraryArray = [];
+
+function Book(bookParameters) {
+    this.author = bookParameters[0];
+    this.title = bookParameters[1];
+    this.numberOfPages = bookParameters[2];
+    this.haveRead = bookParameters[3];
+
+    this.buildBookDOM = function () {
+        let bookDiv = document.createElement("div");
+        let authorText = document.createElement("div");
+
+        bookDiv.classList.add("book");
+
+        authorText.textContent = `${this.author}`;
+        bookDiv.textContent= `${this.title}`;
+
+        bookDiv.append(divContainingBy);
+        bookDiv.append(authorText);
+        library.append(bookDiv);
+    }
 }
 function addingBook(e){
     modalWindow.style.display = "flex";
 }
 function modalSubmit(){
-     let inputs = document.querySelectorAll(".modal > form > label:not([type='radio'])");
-    //  inputs = Array.from(inputs.forEach(input => input.value));
-    //  inputs.join(" ");
+    let inputs = document.querySelectorAll(".modal > form > fieldset > input:not([type='radio'],[type='submit'])");
+    let radioInput = document.querySelector(".modal > form > fieldset > input[type='radio']");
+    let bookParameters = [];
+    inputs.forEach(input => bookParameters.push(input.value));
+    bookParameters.push(radioInput.id);
+    let book = new Book(bookParameters);
+    book.buildBookDOM();
+    libraryArray = Array.from(document.querySelectorAll(".library > .book"));
+    form.reset();
 }
 function closeModal() {
     modalWindow.style.display = "none";
